ab62d3c2c8d410a872144adeaa652720fc37d9bd
mitch <mitch01nguyen@gmail.com>
2022-10-27 10:35:50 -0500

    Adds function to app.js, will return array[2]

--- app.js
+++ app.js
@@ -3,15 +3,22 @@
     In the first stage of this challenge, your task is to use the provided code to make the fetch request and then console log the data.
     If your team has time, update our code using the async/await keywords!
 */
-
+console.log('connected')
 function makeCall() {
   // Use the fetch api to display some simple user data to the console!
   url = "https://reqres.in/api/users";  
 
   
-  fetch() // call the fetch api with the correct input argument!
-    .then() // convert the returned data into a json using the object's built-in methods.
-    .then() // use the resulting json to display some of our user information in the console.
-    .catch(); // console.log an error message in case something goes wrong.
+  fetch(url) // call the fetch api with the correct input argument!
+    .then(res => {
+      if (res.ok){
+        return res.json();
+      }
+    }) // convert the returned data into a json using the object's built-in methods.
+    .then(robert => {
+      const myData = robert.data[2];
+      console.log(myData)
+    }) // use the resulting json to display some of our user information in the console.
+    .catch(console.log); // console.log an error message in case something goes wrong.
 
 };
